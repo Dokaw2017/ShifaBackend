bf1d6b6182abb866db07d195740a24a22ad7e7b0
Nebi <mam10122011@yahoo.com>
2021-05-06 00:32:09 +0300

    piste

--- validators/postValidator.js
+++ validators/postValidator.js
@@ -1,13 +1,13 @@
 import * as yup from "yup";
 
 const title = yup
-  .string()
+  //.string()
   .required("Title of the post is required")
   .min(3, "Title should have atleast 3 charracters")
   .max(100, "Title can have atmost 100 characters");
 
 const description = yup
-  .string()
+  //.string()
   .required("Content of the post is required")
   .min(10, "Content should have atleast 10 characters")
   .max(3000, "Content can have atmost 3000 characters");
